a94607c49030639eaadb9f5ecc6a0796df09d7a6
Konicz, Sebastian P <Sebastian.Konicz@nielsen.com>
2020-08-01 21:41:39 +0200

    Wrote simple fuzzy algorithm for word comparison and came up with new idea for deezer API search

--- src/JTM_deezer_API.py
+++ src/JTM_deezer_API.py
@@ -39,12 +39,6 @@ def main():
         print('saving file')
         df_partial.to_excel(r'C:\Users\kose9001\Desktop\JakaToMelodia\data\interim\deezer_API\ListaPiosenek_{start}-{end}.xlsx'.format(start=truncate_before, end=truncate_after), index=False, encoding='ISO-8859-1')
 
-    # concatenating all dataframes
-    df_all_dataframes = pd.concat(dataframe_list, axis=0, sort=False)
-
-    # saving to excel file
-    print('saving file')
-    df_all_dataframes .to_excel(r'C:\Users\kose9001\Desktop\JakaToMelodia\data\processed\ListaPiosenekAllDeezer.xlsx', index=False, encoding='ISO-8859-1')
 
     # end time of program + duration
     end_time = time.time()
@@ -54,9 +48,6 @@ def deezer_search(search_value):
     # initiating empty variables
     song = ""
     artist = ""
-    album = ""
-    link = ""
-    preview = ""
     try:
         # initiating deezer client
         client = deezer.Client()
@@ -67,37 +58,21 @@ def deezer_search(search_value):
         # search results
         song = search_result_1[0]
         artist = search_result_2.artist
-        album = search_result_2.album
-        link = search_result_2.link
-        preview = search_result_2.preview
     except IndexError:
         pass
     except ValueError:
         pass
-    print(song, artist, album, link, preview)
-    return song, artist, album, link, preview
-
-
-        # getting artist value
-    # df_all['Deezer_Artist_'] = df_all['Song'].map(lambda song: deezer_search_artist(song))
-
-    # # getting deezer preview
-    # df_all['Deezer_Album'] = df_all['Song'].map(lambda song: deezer_search_album(song))
-    #
-    # # getting deezer link
-    # df_all['Deezer_Link'] = df_all['Song'].map(lambda song: deezer_search_link(song))
-    #
-    # # getting deezer preview
-    # df_all['Deezer_Preview'] = df_all['Song'].map(lambda song: deezer_search_preview(song))
+    print(song, artist)
+    return song, artist
 
 def partial_dataframe_creation(df_all):
     result_list = []
     # running search function
     for key, search_value in df_all['Song'].iteritems():
         # search function values
-        song, artist, album, link, preview = deezer_search(search_value)
+        song, artist = deezer_search(search_value)
         # adding result items to list
-        result_list.append([song, artist, album, link, preview])
+        result_list.append([song, artist])
 
     # creating dataframe
     result_df = pd.DataFrame(result_list)
@@ -105,7 +80,7 @@ def partial_dataframe_creation(df_all):
     if result_df.empty == True:
         pass
     else:
-        result_df.columns = ["Deezer_Song", "Deezer_Artist", "Deezer_Album", "Deezer_Link", "Deezer_Preview"]
+        result_df.columns = ["Deezer_Song", "Deezer_Artist"]
 
     # reseting index
     df_all.reset_index(drop=True, inplace=True)
@@ -114,75 +89,6 @@ def partial_dataframe_creation(df_all):
     df_all = pd.concat([df_all, result_df], axis=1, sort=False)
 
     return df_all
-
-# def deezer_search_song(song):
-#     try:
-#         print(song)
-#         client = deezer.Client()
-#         song = client.search(song)
-#         song = song[0]
-#         print(song)
-#     except IndexError:
-#         pass
-#     except ValueError:
-#         pass
-#     return song
-#
-# def deezer_search_artist(song):
-#     artist = ""
-#     try:
-#         print(song)
-#         client = deezer.Client()
-#         song = client.search(song)[0]
-#         performer = song.artist
-#         print(artist)
-#     except IndexError:
-#         pass
-#     except ValueError:
-#         pass
-#     return artist
-#
-# def deezer_search_album(song):
-#     album = ""
-#     try:
-#         print(song)
-#         client = deezer.Client()
-#         song = client.search(song)[0]
-#         album = song.album
-#         print(album)
-#     except IndexError:
-#         pass
-#     except ValueError:
-#         pass
-#     return album
-#
-# def deezer_search_link(song):
-#     link = ""
-#     try:
-#         print(song)
-#         client = deezer.Client()
-#         song = client.search(song)[0]
-#         link = song.link
-#         print(link)
-#     except IndexError:
-#         pass
-#     except ValueError:
-#         pass
-#     return link
-#
-# def deezer_search_preview(song):
-#     preview = ""
-#     try:
-#         print(song)
-#         client = deezer.Client()
-#         song = client.search(song)[0]
-#         preview = song.preview
-#         print(preview)
-#     except IndexError:
-#         pass
-#     except ValueError:
-#         pass
-#     return preview
 
 if __name__ == "__main__":
     main()
