98258becbe5d77b53d16e25e277fac591c2af960
sebastian-konicz <sebastian.konicz@gmail.com>
2020-08-07 16:39:43 +0200

    Changed the search algoritms - added Last FM API

--- src/JTM_08_fuzzywuzzy_match_3.py
+++ src/JTM_08_fuzzywuzzy_match_3.py
@@ -12,7 +12,7 @@ def main():
 
     # loading file
     print('loading file')
-    df_all = pd.read_excel(r'C:\Users\kose9001\Desktop\JakaToMelodia\data\processed\06_ListaPiosenekFuzzy_2.xlsx')
+    df_all = pd.read_excel(r'C:\Users\kose9001\Desktop\JakaToMelodia\data\processed\07_ListaPiosenekFuzzy_2.xlsx')
 
     # Creating new column song/artist_correct_sum with all correct song artist value
     df_all['Song_correct_sum'] = df_all.apply(lambda df_all: match_condition(df_all["Song_correct"], df_all['Song_correct_search']), axis=1)
@@ -30,12 +30,8 @@ def main():
     # replacing NaN values in dataframe
     df_all["Song_correct"].fillna("", inplace=True)
     df_all["Artist_correct"].fillna("", inplace=True)
-    df_all["Song_split"].fillna("", inplace=True)
-    df_all["Artist_split"].fillna("", inplace=True)
 
     # creating new column with concatenated values from song_correct and artist_correct
-    df_all["Split_Concatenated"] = df_all.apply(
-        lambda df_all: (str(df_all['Song_split']) + " " + str(df_all['Artist_split'])), axis=1)
     df_all["Correct_Concatenated"] = df_all.apply(
         lambda df_all: (str(df_all['Song_correct']) + " " + str(df_all['Artist_correct']))
         if (df_all['Song_correct'] != "") & (df_all['Artist_correct'] != "") else "", axis=1)
@@ -47,7 +43,6 @@ def main():
     df_correct.drop_duplicates(inplace=True, keep='first')
     song_dict = pd.Series(df_correct["Song_correct"].values, index=df_correct["Correct_Concatenated"]).to_dict()
     artist_dict = pd.Series(df_correct["Artist_correct"].values, index=df_correct["Correct_Concatenated"]).to_dict()
-
 
     # CHECKING SONG - ARTIST WITH FUZZU RATIO
     df_all["Song_correct_search"] = df_all.apply(
@@ -69,7 +64,7 @@ def main():
 
     # saving to excel file
     print('saving file')
-    df_all.to_excel(r'C:\Users\kose9001\Desktop\JakaToMelodia\data\processed\07_ListaPiosenekFuzzy_3.xlsx', index=False, encoding='ISO-8859-1')
+    df_all.to_excel(r'C:\Users\kose9001\Desktop\JakaToMelodia\data\processed\08_ListaPiosenekFuzzy_3.xlsx', index=False, encoding='ISO-8859-1')
 
     # end time of program + duration
     end_time = time.time()
