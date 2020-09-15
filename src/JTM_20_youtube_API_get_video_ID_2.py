8e1a5f63ffbc11d5c3f09d1b79fcbadd30a2af3f
sebastian-konicz <sebastian-konicz@gmail.com>
2020-09-15 17:22:13 +0200

    Changes in API search - used selenium for scrapping video links'

--- src/JTM_20_youtube_API_get_video_ID_2.py
+++ src/JTM_20_youtube_API_get_video_ID_2.py
@@ -3,9 +3,11 @@ import re
 import pandas as pd
 import time
 import selenium
-from googleapiclient.discovery import build
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
-PATH = 'C:\Users\kose9001\Desktop\JTM'
+PATH = r'C:\Users\kose9001\Desktop\JTM\chromedriver.exe'
 
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
@@ -23,15 +25,12 @@ def main():
     df_all["Song_concat"] = df_all.apply(lambda df: df['Song_correct'] + " " + df['Artist_correct'], axis=1)
 
     # getting youtube video id
-    df_all["YT_id"] = df_all.apply(lambda df: youtube_search(df['Song_concat']), axis=1)
-
-    # link to the song
-    df_all["YT_link"] = df_all.apply(lambda df: "https://www.youtube.com/watch?v=" + df["YT_id"], axis=1)
+    df_all["YT_link"] = df_all.apply(lambda df: youtube_search(df['Song_concat']), axis=1)
 
     # saving to excel file
     print('saving file')
     df_all.to_csv(r'C:\Users\kose9001\Desktop\JakaToMelodia\data\processed\20_ListaPiosenekYoutube.csv',
-                    index=False, encoding='ISO-8859-1')
+                    index=False, encoding='UTF-8') # ISO-8859-1
 
     # end time of program + duration
     end_time = time.time()
@@ -39,30 +38,24 @@ def main():
 
 def youtube_search(search_value):
     try:
-        print(str(search_value))
+        search_value = str(search_value)
 
-        api_key = JTM_0_config.api_key
+        driver = webdriver.Chrome(PATH)
+        driver.get('https://www.youtube.com/results?search_query=' + search_value)
 
-        youtube = build('youtube', 'v3', developerKey=api_key)
+        video = driver.find_element_by_id('video-title')
+        video_link = video.get_attribute('href')
 
-        request = youtube.search().list(
-            part="snippet",
-            maxResults=3,
-            q=str(search_value))
+        print(search_value)
+        print(video_link)
 
-        response = request.execute()
-
-        song_id = response['items'][0]['id']['videoId']
-        # song_title = response['items'][0]['snippet']['title']
-
-        print(response['items'][0]['id']['videoId'])
-        # print(response['items'][0]['snippet']['title'])
+        driver.quit()
 
     except IndexError:
-        song_id = ""
+        video_link = ""
     except ValueError:
-        song_id = ""
-    return song_id
+        video_link = ""
+    return video_link
 
 if __name__ == "__main__":
     main()
